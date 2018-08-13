73c1d6c4e6668216583fbafdaeacc67ec125ece5
vighneshmufc <vigneshnair94@gmail.com>
2018-08-13 15:40:29 +0530

    First commit

---
+++ src/main/java/com/bookstores/BookstoresApplication.java
@@ -0,0 +1,12 @@
+package com.bookstores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoresApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bookstores/BookstoresApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookstores;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookstoresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
